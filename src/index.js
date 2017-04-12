88049b8300d9991ec2864439ddc2e91ce36283d4
Pradel Léo <pradel.leo@gmail.com>
2017-04-12 15:46:21 +0200

    Add eslint

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const request = require('request');
 
-module.exports = function(name, type = 'user') {
+module.exports = function findFacebookId(name, type = 'user') {
   return new Promise((resolve, reject) => {
-    if (!name) throw new Error('Invalid url');
+    if (!name) {
+      reject('Invalid url');
+      return;
+    }
     let url;
     switch (type) {
       case 'user':
@@ -13,7 +16,8 @@ module.exports = function(name, type = 'user') {
         url = `https://www.facebook.com/groups/${name}`;
         break;
       default:
-        throw new Error('invalid type');
+        reject('Invalid type');
+        return;
     }
     request({
       method: 'GET',
@@ -25,7 +29,7 @@ module.exports = function(name, type = 'user') {
       if (err) {
         reject(err);
       } else if (res.statusCode === 200) {
-		    const arrMatches = body.match(/entity_id":"\d*/i);
+        const arrMatches = body.match(/entity_id":"\d*/i);
         if (arrMatches && arrMatches.length > 0) {
           const id = arrMatches[0].split('"').pop();
           resolve(id);
@@ -40,4 +44,4 @@ module.exports = function(name, type = 'user') {
       }
     });
   });
-}
+};
--- src/index.test.js
+++ src/index.test.js
@@ -1,22 +1,21 @@
 const findFacebookId = require('./index.js');
 
-test('should resolve facebook user', () => {
-  return findFacebookId('zuck').then((id) => {
+test('should resolve facebook user', () =>
+  findFacebookId('zuck').then((id) => {
     expect(id).toBe('4');
-  });
-});
+  }));
 
-test('should resolve facebook page', () => {
-  return findFacebookId('laurentgarnierofficial').then((id) => {
+test('should resolve facebook page', () =>
+  findFacebookId('laurentgarnierofficial').then((id) => {
     expect(id).toBe('273706360456');
-  });
-});
+  }));
 
-test('should pass error', () => {
-  return findFacebookId('404notexistonfacebook').then(() => {
-    throw new Error();
-  }).catch((err) => {
-    expect(err.message).toBe('404: id not found for 404notexistonfacebook');
-    expect(err.statusCode).toBe(404);
-  });
-});
+test('should pass error', () =>
+  findFacebookId('404notexistonfacebook')
+    .then(() => {
+      throw new Error();
+    })
+    .catch((err) => {
+      expect(err.message).toBe('404: id not found for 404notexistonfacebook');
+      expect(err.statusCode).toBe(404);
+    }));
